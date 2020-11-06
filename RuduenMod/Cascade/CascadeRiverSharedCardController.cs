1f1389629c5962fe5f94ec86dc995a99d34ea857
Ruduen <45407351+Ruduen@users.noreply.github.com>
2020-11-06 04:55:03 -0600

    Minor fixes for river deck. Reviewing options for design at this time.

---
+++ RuduenMod/Cascade/CascadeRiverSharedCardController.cs
@@ -0,0 +1,30 @@
+﻿using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Workshopping.BreachMage
+{
+    public class CascadeRiverSharedCardController : CardController
+    {
+        public CascadeRiverSharedCardController(Card card, TurnTakerController turnTakerController)
+            : base(card, turnTakerController)
+        {
+        }
+
+        public Location RiverDeck()
+        {
+            return base.TurnTaker.FindSubDeck("RiverDeck");
+        }
+
+        public Location RiverDeckPlayArea()
+        {
+
+            return base.TurnTaker.FindSubPlayArea("RiverDeck");
+
+        }
+
+    }
+}
--- RuduenMod/Cascade/CascadeTurnTakerController.cs
+++ RuduenMod/Cascade/CascadeTurnTakerController.cs
@@ -17,6 +17,7 @@ namespace Workshopping.Cascade
             IEnumerator coroutine;
             // At the start of game, move all River cards into River deck. (This best preserves the 'identity' of the cards.)
             // It's messy, but search through all river cards from Cascade's deck and always move them to the river deck.
+            int handSize = this.NumberOfCardsInHand;
             IEnumerable<Card> riverCards = base.GameController.FindCardsWhere((Card c) => c.DoKeywordsContain("river") && c.Owner == this.TurnTaker);
             coroutine = base.GameController.MoveCards(this, riverCards, this.TurnTaker.FindSubDeck("RiverDeck"));
             if (base.UseUnityCoroutines) { yield return base.GameController.StartCoroutine(coroutine); } else { base.GameController.ExhaustCoroutine(coroutine); }
@@ -31,14 +32,14 @@ namespace Workshopping.Cascade
             //    false, SelectionType.MoveCard, null, false, null, true, false, false, null, null, false, MoveCardDisplay.None, null);
             //if (base.UseUnityCoroutines) { yield return base.GameController.StartCoroutine(coroutine); } else { base.GameController.ExhaustCoroutine(coroutine); }
 
-            // ShuffleCardsIntoLocation doesn't seem to work gravefully.
+            // ShuffleCardsIntoLocation doesn't seem to work gracefully.
             //IEnumerable<Card> riverCards = base.GameController.FindCardsWhere((Card c) => c.DoKeywordsContain("river") && c.Owner == this.TurnTaker);
             //// Find all river cards and shuffle them into the river deck.
             //coroutine = base.GameController.ShuffleCardsIntoLocation(this, riverCards, this.TurnTaker.FindSubDeck("RiverDeck"));
 
-            // Then move 4 cards to hand. Don't draw to avoid animation. This is the start of game, so it just loops. It's messy, but it works for now!
+            // Then move enough replacements to get starting cards to hand. Don't draw to avoid animation. This is the start of game, so it just loops. It's messy, but it works for now!
             // TODO: Seriously, is there a better option?
-            while (this.HeroTurnTaker.NumberOfCardsInHand < 4)
+            while (this.HeroTurnTaker.NumberOfCardsInHand < handSize)
             {
                 coroutine = this.GameController.MoveCard(this, this.HeroTurnTaker.Deck.TopCard, this.HeroTurnTaker.Hand);
                 if (base.UseUnityCoroutines) { yield return base.GameController.StartCoroutine(coroutine); } else { base.GameController.ExhaustCoroutine(coroutine); }
