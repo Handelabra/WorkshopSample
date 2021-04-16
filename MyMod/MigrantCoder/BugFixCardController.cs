20ff0767547c2a2b1f86f19c3478d87b4226bff6
John Arnold <john@handelabra.com>
2021-04-16 17:27:47 -0300

    add another card for migrant coder

---
+++ MyMod/MigrantCoder/BugFixCardController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+namespace Workshopping.MigrantCoder
+{
+    public class BugFixCardController: CardController
+    {
+        public BugFixCardController(Card card, TurnTakerController turnTakerController)
+            : base(card, turnTakerController)
+        {
+        }
+
+        public override IEnumerator UsePower(int index = 0)
+        {
+            // Migrant Coder deals 1 target 4 lightning damage.
+            var numberOfTargets = GetPowerNumeral(0, 1);
+            var damageAmount = GetPowerNumeral(1, 4);
+            IEnumerator dealDamageE = this.GameController.SelectTargetsAndDealDamage(this.DecisionMaker,
+                                                                                     new DamageSource(this.GameController, this.CharacterCard),
+                                                                                     damageAmount,
+                                                                                     DamageType.Lightning,
+                                                                                     numberOfTargets,
+                                                                                     false,
+                                                                                     numberOfTargets,
+                                                                                     cardSource: GetCardSource());
+            if (UseUnityCoroutines)
+            {
+                yield return this.GameController.StartCoroutine(dealDamageE);
+            }
+            else
+            {
+                this.GameController.ExhaustCoroutine(dealDamageE);
+            }
+        }
+    }
+}
